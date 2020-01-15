bd1ab983ce371ed9d299da28570f1d688c9214b7
MFeiz <30654631+mohammadFeiz@users.noreply.github.com>
2020-01-15 14:29:20 +0330

    .

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var _react = _interopRequireWildcard(require("react"));
 
 var _rRangeSlider = _interopRequireDefault(require("r-range-slider"));
 
-var _rCanvas = _interopRequireDefault(require("./r-canvas"));
+var _rCanvas = _interopRequireDefault(require("r-canvas"));
 
 var _jquery = _interopRequireDefault(require("jquery"));
 
