40022a14141472bc1297c66d1a2d6437b58eaead
MFeiz <30654631+mohammadFeiz@users.noreply.github.com>
2020-11-30 17:44:52 +0330

    .

--- functions.js
+++ functions.js
@@ -3,10 +3,10 @@ export function getGap(labels){
   return Math.max(0.5,Math.round(labels.length / 10))
 }
 export function number_getRange(axis,details){
-      var {limit,width,height} = details;
-      var filter = details.filter[axis];
-      var size = {x:width,y:height}[axis];
-      var [min,max] = limit[axis];
+      var {min,max,canvasSize} = details;
+      var filter = details.getFilter[axis]();
+      console.log(filter);
+      var size = canvasSize[axis];
       if(min === undefined || max === undefined){return false;}
       var range = max - min,i = 1;
       var start,step,end;
@@ -30,15 +30,18 @@ export function number_getRange(axis,details){
       return {start:fs,step,end:fe,filter:filteredRange}; 
     } 
     export function string_getRange(axis,details){
-      var filter = details.filter[axis];
-      var {labels,width = 60,height = 30} = details.Axis[axis];
-      var gap = getGap(labels);
-      var size = details[axis === 'x'?'width':'height']
-      var fs = filter[0]?labels.indexOf(filter[0]):0;
-      var fe = filter[1]?labels.indexOf(filter[1]):labels.length - 1;
-      var filteredRange = {start:0,end:labels.length - 1,p1:fs,p2:fe};
+      var filter = details.getFilter[axis]();
+      var labelSize;
+      if(axis === 'x'){labelSize = details.Axis.x.width || 60;}
+      else{labelSize = details.Axis.y.height || 30;}
+      var canvasSize = details.canvasSize[axis]
+      var list = details.KeyAxis.list;
+      var gap = getGap(list);
+      var fs = filter[0]?list.indexOf(filter[0]):0;
+      var fe = filter[1]?list.indexOf(filter[1]):list.length - 1;
+      var filteredRange = {start:0,end:list.length - 1,p1:fs,p2:fe};
       var count = fe - fs + 1;
-      var approveCount = Math.floor(size / (axis === 'x'?width:height));
+      var approveCount = Math.floor(canvasSize / labelSize);
       approveCount = approveCount < 1 ? 1:approveCount;
       var labelStep = Math.floor(count / approveCount);
       labelStep = labelStep < 1 ? 1:labelStep;
@@ -47,50 +50,47 @@ export function number_getRange(axis,details){
       };
     }
 
-    export function getPixelByValue(value,axis){
-      return this.getPercentByValue(value,axis) * this.details[axis === 'x'?'width':'height'] / 100;
-    }
+    // export function getPixelByValue(value,axis){
+    //   return this.getPercentByValue(value,axis) * this.details[axis === 'x'?'width':'height'] / 100;
+    // }
 
-    export function getPercentByValue(value,axis,details){
-      var {range,type} = details;
-      let labels = details.Axis[axis].labels;
+    export function number_getPercentByValue(value,axis,details){
+      var {range} = details;
       let {start,end} = range[axis],Value; 
-      if(type[axis] === 'number'){
-        if(isNaN(value)){return 'number error'}  
-        Value = value; 
-      }
-      else{
-        Value = labels.indexOf(value);
-        if(Value === -1){return 'string error';}
-      }
+      if(isNaN(value)){return false}  
+      Value = value; 
       return 100 * (Value - start) / (end - start) 
     }
-    export function getValueByPercent(p,axis,details){
-      var {range,type,Axis,precision} = details;
+    export function string_getPercentByValue(index,axis,details){
+      var {range} = details;
+      let {start,end} = range[axis]; 
+      return 100 * (index - start) / (end - start) 
+    }
+    export function number_getValueByPercent(p,axis,details){
+      var {range,precision} = details;
       if(!range[axis]){return '';}
       let {start,end} = range[axis],Value = (end - start) * p / 100;
-      if(axis === 'x'){console.log(start,end)}
-      if(type[axis] === 'number'){return parseFloat((Value + start).toFixed(precision))}
-      else{
-        let labels = Axis[axis].labels;
-        var a = Math.round(Value + start);
-        console.log('value',a)
-        console.log('p',p)
-        return labels[a]
-      }
+      return parseFloat((Value + start).toFixed(precision))
     }
-
-    export function getLimitTypeNumber(data,axis){
+    export function string_getValueByPercent(p,axis,details){
+      var {range,KeyAxis} = details;
+      if(!range[axis]){return '';}
+      let {start,end} = range[axis],Value = (end - start) * p / 100;
+      var a = Math.round(Value + start);
+      return KeyAxis.list[a]
+    }
+    export function getLimitTypeNumber(data){
       var min = Infinity,max = -Infinity;
       for (var i = 0; i < data.length; i++) {
-        var {stream = []} = data[i];
-        for (var j = 0; j < stream.length; j++) { 
-          var value = stream[j][axis]; 
+        var {points = []} = data[i];
+        for (var j = 0; j < points.length; j++) { 
+          let point = points[j];
+          var value = this.getValue(point); 
           if(value < min){min = value;}
           if(value > max){max = value;}
         }
       }
-      return [min === Infinity?undefined:min,max === -Infinity?undefined:max];
+      return {min,max};
     }
 
     export function eventHandler(selector, event, action,type = 'bind'){
@@ -99,4 +99,37 @@ export function number_getRange(axis,details){
       var element = typeof selector === "string"?(selector === "window"?$(window):$(selector)):selector; 
       element.unbind(event, action); 
       if(type === 'bind'){element.bind(event, action)}
+    }
+    export function getShapes(shapes,details){
+      var Shapes = [];
+      for(var i = 0; i < shapes.length; i++){
+        let shape = shapes[i]
+        let obj = {...shape};
+        if(shape.points){
+          obj.points = [];
+          for(var j = 0; j < shape.points.length; j++){
+            let {x,y} = shape.points[j];
+            obj.points.push([details.getPercentByValue.x(x) + '%',-details.getPercentByValue.y(y) + '%'])
+          }
+        }
+        else if(shape.r){
+          let {x,y} = shape;
+          obj.x = details.getPercentByValue.x(x) + '%';
+          obj.y = -details.getPercentByValue.y(y) + '%';   
+        }
+        Shapes.push(obj)  
+      }
+      return Shapes;
+    }
+    export function translate(value){
+      var dictionary = {
+        'Add.Point':{en:'Add Point',fa:'افزودن نقطه'},
+        'Edit.Point':{en:'Edit Point',fa:'ویرایش نقطه'},
+        'Remove.Point':{en:'Remove Point',fa:'حذف نقطه'},
+        'Add':{en:'Add',fa:'افزودن'},
+        'Edit':{en:'Edit',fa:'ویرایش'},
+        'Remove':{en:'Remove',fa:'حذف'},
+        'Close':{en:'Close',fa:'بستن'},
+      }
+      return dictionary[value][this.props.globalization];
     }
